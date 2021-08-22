d053f3e3d5d3e0617895981d1e27b945ce665303
Flashminat0 <wyasaslowe@gmail.com>
2021-08-22 09:56:56 +0530

    done and working

---
+++ app/src/main/java/com/flashminat0/labsheet/MainActivity.java
@@ -0,0 +1,38 @@
+package com.flashminat0.labsheet;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.media.MediaPlayer;
+import android.os.Bundle;
+import android.provider.Settings;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+        Button btnStart = (Button) this.findViewById(R.id.btnStart);
+        btnStart.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startService(new Intent(MainActivity.this, MyServices.class));
+            }
+        });
+
+        Button btnStop = (Button) this.findViewById(R.id.btnStart);
+        btnStop.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                stopService(new Intent(MainActivity.this, MyServices.class));
+            }
+        });
+    }
+
+
+}
