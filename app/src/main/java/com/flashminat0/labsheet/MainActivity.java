67409d2478a87f3997aa5482d59a00463cb47f9e
Flashminat0 <wyasaslowe@gmail.com>
2021-08-22 09:57:58 +0530

    Optimized imports

--- app/src/main/java/com/flashminat0/labsheet/MainActivity.java
+++ app/src/main/java/com/flashminat0/labsheet/MainActivity.java
@@ -1,13 +1,11 @@
 package com.flashminat0.labsheet;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
-import android.media.MediaPlayer;
 import android.os.Bundle;
-import android.provider.Settings;
 import android.view.View;
 import android.widget.Button;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
